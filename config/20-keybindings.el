a677ca5f2d5309a6d4fb8663d4edf6bc11168c36
Artemiy Solopov <art-solopov@yandex.ru>
2015-04-27 19:36:27 +0300

    Windmove keybindings

---
+++ config/20-keybindings.el
@@ -0,0 +1,6 @@
+;; Keybindings for builtin Emacs functions
+
+(global-set-key (kbd "C-x <M-up>") 'windmove-up)
+(global-set-key (kbd "C-x <M-down>") 'windmove-down)
+(global-set-key (kbd "C-x <M-left>") 'windmove-left)
+(global-set-key (kbd "C-x <M-right>") 'windmove-right)
