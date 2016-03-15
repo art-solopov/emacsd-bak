4e1fcb94f027f00c5b3079f8dea069ec088b4724
Artemiy Solopov <a.solopov@socialquantum.ru>
2016-03-15 19:28:27 +0300

    Copy keybindings

--- config/20-keybindings.el
+++ config/20-keybindings.el
@@ -32,5 +32,26 @@
 ;; Imenu
 (global-set-key (kbd "C-c f") 'imenu)
 
+;; "Killing" various stuff
+(defun msg-and-kill (stuff)
+  (message stuff)
+  (kill-new stuff)
+  )
+
 ;; Copy current file path
-(global-set-key (kbd "C-c @") (lambda () (interactive) (kill-new buffer-file-name)))
+(defun copy-full-file-path ()
+  "Copy full file path"
+  (interactive)
+  (msg-and-kill buffer-file-name)
+  )
+(defun copy-local-file-path ()
+  "Copy project-local (projectile-local) file path"
+  (interactive)
+  (msg-and-kill
+   (s-replace (projectile-project-root) "" buffer-file-name)
+   )
+  )
+
+
+(global-set-key (kbd "C-c 2") 'copy-local-file-path)
+(global-set-key (kbd "C-c @") 'copy-full-file-path)
